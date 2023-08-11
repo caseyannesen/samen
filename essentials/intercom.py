9f276422b957a37745d3f63737da7e7f9c27cfcf
Casey <caseylaurensen@gmail.com>
2023-08-12 01:20:33 +0200

    second commit

--- essentials/intercom.py
+++ essentials/intercom.py
@@ -9,15 +9,15 @@ import json, time
 MESSAGE_TEMPLATE = {"id": "", "type": "", "from": "", "to": "", "timestamp": "", "data": {}, "cache_id": ""}
 
 BROKER_DATA = {
-    'username':"", 'password':"", 'subscribe_to': [], 'publish_to': [], 
+    'username':"onverantwoordelik", 'password':"asdf8090ABC!!", 'subscribe_to': ['testin'], 'publish_to': ['testout'], 
     'address':'3a4f7d6b0cd1473681d6c9bdfa569318.s2.eu.hivemq.cloud',
     'mqtt_port':8883, 'ws_port':8884, 'use_websockets':False,
-    'timeout':3600, "subscribe_to": [], "publish_to": [], "client_id": ""
+    'timeout':3600, "client_id": "testin"
 }
 
 
 class MqttMessageHandler:
-
+    # initializes the client
     def __init__(self, broker_data=BROKER_DATA, call_backs={}, my_id="") -> None:
         if broker_data:
             self.client = self.get_client()
@@ -28,7 +28,7 @@ class MqttMessageHandler:
             self.on_connect = call_backs.get('on_connect', self.on_connect)
             self.on_message = call_backs.get('on_message', self.on_message)
             self.on_disconnect = call_backs.get('on_disconnect', self.on_disconnect)
-
+    #returns a client instance
     def get_client(self, broker_data=None, clean_session=True):
         resp = False
         if broker_data: # Create a client instance
@@ -52,7 +52,7 @@ class MqttMessageHandler:
                     client.disconnect()
             resp = client
         return resp
-    
+    #notifies connetion to subscribers.
     def on_connect(self, client, userdata, flags, rc):
         if self.broker_data.get('broker_data') and rc == 0:
             self.subscribe_to_topics(self.broker_data['subscribe_to'])
@@ -60,32 +60,40 @@ class MqttMessageHandler:
             self.publish_to_topics(self.broker_data['publish_to'], message)
 
     def on_disconnect(self, client, userdata, rc):
-        if self.broker_data.get('broker_data'):
-            self.subscribe_to_topics(self.broker_data['subscribe_to'])
-            self.publish_to_topics(self.broker_data['publish_to'])
-
+        pass
+    # handles all the messages received
     def on_message(self, client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
-    
+    #subscribes to all the topics
     def subscribe_to_topics(self, topics):
         for topic in topics:
             self.client.subscribe(topic)
-
+    #publishes to all the topics
     def publish_to_topics(self, topics, message):
         for topic in topics:
-            self.client.publish(topic, payload="Hello", qos=0, retain=False)
-
+            self.client.publish(topic, payload=json.dumps(message), qos=2, retain=True)
+    #starts the client
     def start(self):
         if self.client:
             self.client.loop_start()
-
+    #stops the client
     def stop(self):
         if self.client:
             self.client.loop_stop()
-
+    #prepares the message to be sent
     def prepare_message(self, message_data, cache=None):
         message = MESSAGE_TEMPLATE.copy()
         message.update({'timestamp': time.time(), 'id': self.my_id})
         message.update(message_data)
         return message
     
+    def send_message(self, message="", type="notice", to="all", cache=None):
+        message = self.prepare_message({'type': type, 'from': self.my_id, 'to': to, 'data': {'message': message}}, cache=cache)
+        self.publish_to_topics(self.broker_data['publish_to'], message)
+
+if __name__ == '__main__':
+    print('test client for mqtt message handler')
+    message_handler = MqttMessageHandler()
+    message_handler.start()
+    while True:
+        message_handler.send_message(message=input("enter message: "), type="notice", to="all")
